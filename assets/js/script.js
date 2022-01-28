34a587ddf8516aa685920762b683f9ad919cbf5b
Johann Pouponnot <johannpouponnot@mbp-de-johann.lan>
2022-01-28 15:19:02 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //   console.log("Le document est chargé");
+  const contactForm = document.querySelector("#contactForm");
+  contactForm.addEventListener("submit", async (el) => {
+    el.preventDefault();
+    const data = {
+      firstname: document.querySelector("#firstname").value,
+      lastname: document.querySelector("#lastname").value,
+      email: document.querySelector("#email").value,
+      subject: document.querySelector("#subject").value,
+      message: document.querySelector("#message").value,
+    };
+    console.log(data);
+  });
+});
